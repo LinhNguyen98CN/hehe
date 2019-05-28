375183a46041aa510679c8b3022c78e0abe928b1
LinhNguyen98CN <linhnguyen9806@gmail.com>
2019-05-28 13:51:32 +0700

    bài web điện thoại

---
+++ WebBanDT/WebBanDT/WebBanDT/Controllers/CustomersController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models;
+using WebBanDT.Models.Data;
+using WebBanDT.Models.Function;
+using System.IO;
+
+namespace WebBanDT.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public int MyCustomer { get; set; }
+        BanDT1 db = new BanDT1();
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            ViewBag.customersid = new SelectList(db.Customers.ToList(), "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Customer customer, HttpPostedFileBase fileupload)
+        {
+            string filename = "";
+            //lấy id lớn nhất rồi cộng thêm 1
+            int lastId = int.Parse(db.Customers.ToList().OrderBy(e => int.Parse(e.Id.Trim())).Last().Id.Trim()) + 1;
+            customer.Id = lastId.ToString();
+            if (fileupload != null)
+            {
+                filename = Path.GetFileName(fileupload.FileName);
+                var path = Path.Combine(Server.MapPath("~/imageTel"), filename);
+                if (System.IO.File.Exists(path))
+                {
+
+                    filename = DateTime.Now.ToString("yyyyMMddHHmmssfff") + "_" + filename;
+                    path = Path.Combine(Server.MapPath("~/imageTel"), filename);
+                }
+                fileupload.SaveAs(path);
+            }
+            customer.Name = filename;
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            
+
+            var cart = (Cart)Session["CartSession"];
+            if (cart != null)
+            {
+                cart.Clear();
+                //Gán vào session
+                Session["CartSession"] = cart;
+            }
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WebBanDT/WebBanDT/WebBanDT/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBanDT.Models.Data;
+
+namespace WebBanDT.Controllers
+{
+    public class HomeController : Controller
+    {
+        BanDT1 db = new BanDT1();
+        // GET: Home
+        public ActionResult Index(int? pageTemp, string searchString)
+        {
+            var phones = from l in db.Phones
+                         select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                phones = phones.Where(s => s.Name.Contains(searchString));
+            }
+            int pageSize = 5;
+            int pageNumber = pageTemp ?? 1;
+            return View(phones.ToList().OrderBy(a => a.Id).ToPagedList(pageNumber, pageSize));
+
+
+        }
+        [HttpGet]
+        public ActionResult Search(string searchString)
+        {
+            var phones = from l in db.Phones
+                         select l;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                phones = phones.Where(s => s.Name.Contains(searchString));
+
+            }
+            return View(phones);
+
+        }
+    }
+}
